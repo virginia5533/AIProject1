55ac09b096e00deb115c989475d524a6d7113099
virginia5533 <vlv5@students.uwf.edu>
2023-02-04 20:07:23 -0600

    Getting M and verify player move is in range

--- Main.java
+++ Main.java
@@ -1,5 +1,90 @@
+import java.util.Scanner;
+
 public class Main {
+
   public static void main(String[] args) {
-    System.out.println("Welcome to Connect M!");
+
+    Scanner scanner = new Scanner(System.in);
+    Boolean verifyM = false;
+    Boolean gameOver = false;
+    Boolean validPlay = false;
+    String answer = "";
+    int M = -1;
+
+    System.out.println(
+        "+==================================================+\n" +
+        "|   _____                            _     __  __  |\n" +
+        "|  / ____|                          | |   |  \\/  | |\n" +
+        "| | |     ___  _ __  _ __   ___  ___| |_  | \\  / | |\n" +
+        "| | |    / _ \\| '_ \\| '_ \\ / _ \\/ __| __| | |\\/| | |\n" +
+        "| | |___| (_) | | | | | | |  __/ (__| |_  | |  | | |\n" +
+        "|  \\_____\\___/|_| |_|_| |_|\\___|\\___|\\__| |_|  |_| |\n" +
+        "+==================================================+\n");
+
+ do {   
+       
+  
+  System.out.println("Please enter how big you would like the board in the form of ONE integer\n" + "(For example, 3 generates 3 x 3) ");
+  M = scanner.nextInt();
+  
+  scanner.nextLine();
+
+  for(int i = 0; i < M*2 + 1; i++){
+    System.out.print("-");
   }
+  System.out.println("");
+  for(int i = 0; i < M; i++){
+  for(int x = 0; x < M; x++){
+    System.out.print("| ");
+  }
+  System.out.println("|");
+  for(int y = 0; y < M*2+1; y++){
+    System.out.print("-");
+  }
+  System.out.println("");
+  }
+  
+  System.out.println("Is a " + M + " x " + M + " board ok? (y/n):");
+  
+  answer = scanner.nextLine();
+  
+
+  if(answer.equals("yes") == true){
+  
+    verifyM = true;
+     
+    
+  }
+
+} while(!verifyM);
+  /*System.out.println(M);*/
+
+  System.out.println("Alright! Make the first move by entering the column number:");
+  while(!gameOver){
+  
+
+  do {
+
+    System.out.println("*PLAYER MOVE*");
+    int playColumn = scanner.nextInt();
+    scanner.nextLine();
+
+    if(playColumn < M){
+        validPlay = true;
+    }
+    else {
+        System.out.println("Sorry, that column is out of range. Pick a column between 1 and " + M);
+    }
+
+    
+  }while(!validPlay);
+
+  System.out.println("/..Implementing Move../");
+
+  }//while game loop
+           
+  scanner.close();
+  
+}
+
 }

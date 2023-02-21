86f541679e8eeb0d59b85d2ec6e1e21a3688712e
adam <spencej.adam@gmail.com>
2023-02-21 10:00:00 -0600

    check win logic

--- CheckWin.java
+++ CheckWin.java
@@ -1,26 +1,85 @@
 public class CheckWin {
-    
-    public boolean CheckForWin(int[][] board, char player, int M) {
+    int[][] board;
+    char player;
+    int M, N;
 
-        int checkM = 0;
-        for(int col = M; col > 0; col--){
-
-        for(int row = M; row > 0; row--){
-
-            if(board[row][col] == player){
-                checkM ++;
-            }
-            else{
-                checkM = 0;
-            }
-
-
-
-        }
-        if(checkM == M){
-            return true;
-        }
+    public CheckWin(int[][] board, char player, int M, int N) {
+        this.board = board;
+        this.player = player;
+        this.M = M;
+        this.N = N;
     }
+
+    public boolean CheckForWin() {
+
+        if (CheckVertical() || CheckHorizontal() || CheckDiagonal()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private boolean CheckVertical() {
+
+        // Check for vertical win
+        for (int col = 0; col < N; col++) {
+            int checkM = 0;
+            for (int row = 0; row < N; row++) {
+
+                if (board[row][col] == player) {
+                    checkM++;
+                    if (checkM == M) {
+                        return true;
+                    }
+                } else {
+                    checkM = 0;
+                }
+
+            }
+
+        }
+        return false;
+    }
+
+    private boolean CheckHorizontal() {
+        // Check for Horizontal win
+        for (int row = 0; row < N; row++) {
+            int checkM = 0;
+            for (int col = 0; col < N; col++) {
+
+                if (board[row][col] == player) {
+                    checkM++;
+                    if (checkM == M) {
+                        return true;
+                    }
+                } else {
+                    checkM = 0;
+                }
+
+            }
+
+        }
+        return false;
+    }
+
+    private boolean CheckDiagonal() {
+        // Check for Diagonal win
+        for (int row = 0; row <= N - M; row++) {
+            for (int col = 0; col <= N - M; col++) {
+                int checkM = 0;
+                for (int i = 0; i < M; i++) {
+                    if (board[row + i][col + i] == player) {
+                        checkM++;
+                        if (checkM == M) {
+                            return true;
+                        }
+                    } else {
+                        checkM = 0;
+                    }
+                }
+            }
+        }
 
         return false;
     }

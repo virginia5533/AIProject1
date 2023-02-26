f535a476802d9863be9e07073c9f823904995b30
adam <spencej.adam@gmail.com>
2023-02-26 17:56:34 -0600

    Added javadoc

--- AlphaBeta.java
+++ AlphaBeta.java
@@ -13,6 +13,16 @@ public class AlphaBeta {
         this.M = M;
     }
 
+    /**
+     * This function is a recursive function to find the best possible move
+     * 
+     * @param board takes in current board
+     * @param depth how far the recursive is going
+     * @param alpha the alpha for alphabeta pruning
+     * @param beta the beta for alphabeta pruning
+     * @param player which player is next move
+     * @return alpha or beta depending on board and search, return the col to play
+     */
     public int alphabeta(int[][] board, int depth, int alpha, int beta, int player) {
         CheckWin checkWin = new CheckWin(board, player);
 
@@ -47,6 +57,11 @@ public class AlphaBeta {
         return alpha;
     }
     
+    /**
+     * this function checks the total possible winning moves
+     * @param board takes in current board
+     * @return numWins
+     */
     public int evaluate(int[][] board) {
         // Count the number of possible winning moves for the AI player
         CheckWin checkWin = new CheckWin(board, aiPlayer, 2, N);
@@ -65,6 +80,12 @@ public class AlphaBeta {
     }
 
     //simple check if valid AI move
+    /**
+     * 
+     * @param board takes in current board
+     * @param input takes in input move
+     * @return true or false depending if move is legal
+     */
     public boolean isValidMove(int[][] board, int input){
 
         if(board[0][input] != 0 || (input < 0 || input >= board.length)){
@@ -77,6 +98,13 @@ public class AlphaBeta {
 
     //make move function for AI to place moves
     //Should only be changing this local game board until best move is found
+    /**
+     * 
+     * @param board takes in current board
+     * @param col takes in the chosen col
+     * @param player takes in which player
+     * @return updated board
+     */
     public int makeMove(int[][] board, int col, int player){
         int row = board.length -1;
         while(row >= 0 && board[row][col] != 0){
@@ -89,6 +117,12 @@ public class AlphaBeta {
     }
     
     //undo move 
+    /**
+     * 
+     * @param board takes in current board
+     * @param row takes in row
+     * @param col takes in col
+     */
     public void undoMove(int[][]board, int row, int col){
         board[row][col] = 0;
     }

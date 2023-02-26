98721722cb18dd874cd5177028f2c69781ddee08
adam <spencej.adam@gmail.com>
2023-02-26 15:23:11 -0600

    added comment

--- CheckWin.java
+++ CheckWin.java
@@ -15,6 +15,7 @@ public class CheckWin {
         this.player = player;
     }
 
+    //return boolean if win is true
     public boolean CheckForWin() {
 
         if (CheckVertical() || CheckHorizontal() || CheckDiagonalLeftToRight() || CheckDiagonalRightToLeft()) {
